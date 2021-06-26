7793b29cfc0e091dc7b4b09ff94649b682d0af2d
9074735450 <86360841+9074735450@users.noreply.github.com>
2021-06-26 20:37:23 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./Header";
+import Info from "./Info";
+import Note from "./Note";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Info />
+      <Info />
+      <Note />
+      <Info />
+      <Info />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> My Bootcamp 1</h1>
+      <p>This is my first bootcamp</p>
+    </div>
+  );
+}
+
+export default Note;
